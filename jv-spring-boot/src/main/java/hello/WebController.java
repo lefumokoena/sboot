0525360c7e3c5972b30a94ff5c2155e97c76a57f
sehlotho@gmail.com <sehlotho@gmail.com>
2017-05-01 03:22:47 -0400

    commit

--- jv-spring-boot/src/main/java/hello/WebController.java
+++ jv-spring-boot/src/main/java/hello/WebController.java
@@ -5,7 +5,6 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
-
 import java.util.List;
 
 import javax.validation.Valid;
@@ -25,11 +24,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
-
 
 import utility.UtilityConfig;
 
@@ -69,12 +66,15 @@ public class WebController {
 			try {
 				JsonNode headNode = mapper.readTree(nw.getBody().toString());
 				String tokenNew = headNode.get("token").asText();
-
+				
 				ResponseEntity projects = getProjetcs("", tokenNew);
 
 				List<Project> p = new ArrayList<>();
 				List<Task> t = new ArrayList<>();
-
+				
+				SimpleDateFormat formatDate = new SimpleDateFormat(
+						"DD-MM-YYYY");
+				
 				JSONArray project_array = new JSONArray(projects.getBody()
 						.toString());
 
@@ -83,15 +83,11 @@ public class WebController {
 
 					Project ps = new Project();
 
-					String sd = projectObject.getString("start_date");
-					SimpleDateFormat formatStartDate = new SimpleDateFormat(
-							"DD-MM-YYYY");
-					Date startDate = formatStartDate.parse(sd);
+					String sd = projectObject.getString("start_date");					
+					Date startDate = formatDate.parse(sd);
 
-					String ed = projectObject.getString("end_date");
-					SimpleDateFormat formatEndDate = new SimpleDateFormat(
-							"DD-MM-YYYY");
-					Date endDate = formatEndDate.parse(sd);
+					String ed = projectObject.getString("end_date");					
+					Date endDate = formatDate.parse(ed);
 
 					ps.setPk((Integer) projectObject.getInt("pk"));
 					ps.setTitle((String) projectObject.getString("title"));
@@ -118,10 +114,8 @@ public class WebController {
 								Task ts = new Task();
 								Project projectTaskData = new Project();
 
-								String dd = projectObject.getString("end_date");
-								SimpleDateFormat formatDueDate = new SimpleDateFormat(
-										"DD-MM-YYYY");
-								Date dueDate = formatDueDate.parse(sd);
+								String dd = projectObject.getString("end_date");								
+								Date dueDate = formatDate.parse(sd);
 
 								ts.setId((Integer) taskObject.getInt("id"));
 								ts.setTitle((String) taskObject
@@ -137,10 +131,22 @@ public class WebController {
 
 								if (projectTaskDataObject != null) {
 
-									projectTaskData
-											.setPk((Integer) projectTaskDataObject
-													.getInt("pk"));
+									String pd_sd = projectTaskDataObject.getString("start_date");									
+									Date pd_startDate = formatDate.parse(pd_sd);
 
+									String pd_ed = projectTaskDataObject.getString("end_date");
+									Date pd_endDate = formatDate.parse(pd_sd);
+									
+									
+									projectTaskData.setPk((Integer) projectTaskDataObject.getInt("pk"));
+									projectTaskData.setTitle((String) projectTaskDataObject.getString("title"));
+									projectTaskData.setStart_date(pd_startDate);
+									projectTaskData.setEnd_date(pd_endDate);
+									projectTaskData.setIs_billable((Boolean) projectTaskDataObject
+											.getBoolean("is_billable"));
+									projectTaskData.setIs_active((Boolean) projectTaskDataObject
+											.getBoolean("is_active"));
+										
 									ts.setProject_data(projectTaskData);
 								}
 
@@ -167,9 +173,26 @@ public class WebController {
 							List<Resource> rList = new ArrayList<>();
 							Resource resource = new Resource();
 
-							resource.setId((Integer) resourceObject
-									.getInt("id"));
-
+							String rs_sd = resourceObject.getString("start_date");
+							Date rs_startDate = formatDate.parse(rs_sd);
+							
+							String rs_ed = resourceObject.getString("end_date");
+							Date rs_endDate = formatDate.parse(rs_ed);
+							
+							String rs_c = resourceObject.getString("created");
+							Date rs_created = formatDate.parse(rs_c);
+							
+							String rs_u = resourceObject.getString("updated");
+							Date rs_updated = formatDate.parse(rs_u);
+							
+							resource.setId((Integer) resourceObject.getInt("id"));
+							resource.setUser((String)resourceObject.getString("user"));
+							resource.setStart_date(rs_startDate);
+							resource.setEnd_date(rs_endDate);
+							resource.setAgreed_hours_per_month((Double)resourceObject.getDouble("agreed_hours_per_month"));
+							resource.setCreated(rs_created);
+							resource.setUpdated(rs_updated);
+							
 							rList.add(resource);
 							ps.setResource_set(rList);
 						}
